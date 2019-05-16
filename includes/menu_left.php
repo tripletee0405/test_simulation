4048dddfa3e0be314f1e982b649a090b03fd73e1
tripletee0405 <42493053+tripletee0405@users.noreply.github.com>
2019-05-16 09:56:59 +0700

    Update slider, menu_left, css

--- includes/menu_left.php
+++ includes/menu_left.php
@@ -2,7 +2,7 @@
 	<h3>Menu</h3>
 	<ul>
 		<?php 
-		$result = mysqli_query($conn, "SELECT * FROM news_catalog");
+		$result = mysqli_query($conn, "SELECT * FROM news_catalog ORDER BY id_catalog ASC");
 		while ($row = mysqli_fetch_assoc($result)){
 			echo "<li><a href='#'>" . $row['name_catalog'] . "</a></li>";
 		}
--- includes/slider.php
+++ includes/slider.php
@@ -1,9 +1,10 @@
 <div id="slider" class="slider">
-	<img class="mySlides" src="https://www.w3schools.com/w3css/img_snowtops.jpg">
-	<img class="mySlides" src="https://www.w3schools.com/w3css/img_lights.jpg">
-	<img class="mySlides" src="https://www.w3schools.com/w3css/img_mountains.jpg">
-	<img class="mySlides" src="https://www.w3schools.com/w3css/img_forest.jpg">
-	<button class="btn_slider_left" onclick="plusDivs(-1)">&#10094;</button>
-	<button class="btn_slider_right" onclick="plusDivs(1)">&#10095;</button>
-</div>
-<script type="text/javascript" src="js/slider.js"></script>
+	<?php 
+	$result = mysqli_query($conn, "SELECT * FROM img_banner");
+	while ($row = mysqli_fetch_assoc($result)){
+		echo "<img class='mySlides' src='" . $row['link_img'] . "'>";	}
+		?>
+		<button class="btn_slider_left" onclick="plusDivs(-1)">&#10094;</button>
+		<button class="btn_slider_right" onclick="plusDivs(1)">&#10095;</button>
+	</div>
+	<script type="text/javascript" src="js/slider.js"></script>
