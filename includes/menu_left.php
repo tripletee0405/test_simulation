a52ca1d99b7cb1c036613fc04133b6fee3adb8e0
tripletee0405 <42493053+tripletee0405@users.noreply.github.com>
2019-05-16 10:49:14 +0700

    Update

--- includes/menu_left.php
+++ includes/menu_left.php
@@ -4,7 +4,7 @@
 		<?php 
 		$result = mysqli_query($conn, "SELECT * FROM news_catalog ORDER BY id_catalog ASC");
 		while ($row = mysqli_fetch_assoc($result)){
-			echo "<li><a href='#'>" . $row['name_catalog'] . "</a></li>";
+			echo "<li><a href='danh_muc.php?id=" . $row['id_catalog'] . "'>" . $row['name_catalog'] . "</a></li>";
 		}
 		?>
 	</ul>
