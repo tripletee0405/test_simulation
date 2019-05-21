6f976e36c36d8854e6d9092bf4a67c8d8da5141d
tripletee0405 <42493053+tripletee0405@users.noreply.github.com>
2019-05-21 23:15:08 +0700

    Update add_catalog.php

--- admin/add_catalog.php
+++ admin/add_catalog.php
@@ -6,16 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     # Thêm danh mục
 	$insert_sql = "INSERT INTO `news_catalog`(`name_catalog`) VALUES ('$catalog')";
+
+	if ($conn->query($insert_sql) === TRUE){
+		$conn->close();
+	}
 }
-$conn->close();
 ?>
 <div>
 	<h2>Thêm danh mục</h2>
 	<form action="add_catalog.php" method="post">
 		<div class="form-group">
-			<input type="text" class="form-control" id="catalog" name="catalog" required>
+			<input type="text" id="catalog" name="catalog">
 		</div>
-		<button type="submit" class="btn btn-primary">Thêm</button>
+		<button type="submit">Thêm</button>
 	</form>
 </div>
 <br>
