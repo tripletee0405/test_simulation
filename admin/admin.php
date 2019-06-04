7f744110b7241231c5a72d660a1f27ac48b25334
tripletee0405 <42493053+tripletee0405@users.noreply.github.com>
2019-06-04 11:21:16 +0700

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -10,6 +10,7 @@
 		<h2>Admin Page</h2>
 	</div>
 	<div class="option_admin">
+		<input type="submit" value="Trang chủ" onclick="location.href='../index.php'">
 		<input type="submit" value="Thêm chuyên mục" onclick="location.href='add_catalog.php'">
 		<input type="submit" value="Thêm bài viết" onclick="location.href='add_news.php'">
 		<input type="submit" value="Danh sách chuyên mục" onclick="location.href='list_catalog.php'">
